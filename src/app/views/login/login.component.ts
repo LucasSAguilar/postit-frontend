a46bcde84d681513c6f98ada4c1a1c2d2f017a55
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-11-27 14:12:48 -0300

    Configuração roteamento

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {}
+
+  login() {
+    console.log("aqui");
+    
+    // Após o login, redireciona para a página Home
+    this.router.navigate(['/home']);
+  }
 }
