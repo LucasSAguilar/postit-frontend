c1a22e918f8cd65dc620c656b180bc7248525d35
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-11-29 14:13:43 -0300

    Troca de equipamento (verificar proximo PR)

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { PostBoxComponent } from "../../components/post-box/post-box.component";
+import {MatGridListModule} from "@angular/material/grid-list"
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PostBoxComponent],
+  imports: [PostBoxComponent, MatGridListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
 
 }
